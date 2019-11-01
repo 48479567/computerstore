aa83ccaf6b60efea923bd5e8360479d410d419b2
48479567 <48479567.sjep@gmail.com>
2019-11-01 18:16:55 -0500

    Add field control with object entry, internet, Fri Nov 01 2019 18:16:46

--- frontend/src/app/shared/components/form/dynamic-scalable-form/dynamic-scalable-form.component.ts
+++ frontend/src/app/shared/components/form/dynamic-scalable-form/dynamic-scalable-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-scalable',
@@ -7,12 +7,25 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 
 export class DynamicFormScalableComponent implements OnInit {
-  @Input() section = { a: 'a', b: 'B', c: 'C'};
+  object = { a: 'a', b: 'b', c: 'c' };
+  @Input() section: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    fullname: new FormControl('', [Validators.email, Validators.required, Validators.minLength(5)])
+  });
   @Input() form: FormGroup;
 
 
   constructor() { }
 
   ngOnInit() {
+    this.form.addControl('section', this.section);
+    console.log('form', this.section.controls);
+  }
+
+  errorInput(name) {
+    if (!name) {
+      return;
+    }
+    return Object.keys(name);
   }
 }
